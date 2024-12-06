b809d92539070f65f64b981ccce667b788cb099f
Chanatip Deemee <78657820+Loxyez@users.noreply.github.com>
2024-12-06 15:56:52 +0700

    Set draft of Home

---
+++ homesavvy/src/pages/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Table from '../components/Table';
+import Form from '../components/Form';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Form />
+      <Table />
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ homesavvy/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Defect Tracker for 1088/45</h1>
+            <p>
+                This is the home page for the Defect Tracker for 1088/45. You can use this app to track defects in your projects.
+            </p>
+        </div>
+    );
+}
